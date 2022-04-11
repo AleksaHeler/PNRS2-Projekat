6be6899cf69f5ba9de1871ae9dee225f3d16c8b7
TrAceX <djole1271952@gmail.com>
2022-04-11 16:03:44 +0200

    Interface

--- bench/UVM/interface.sv
+++ bench/UVM/interface.sv
@@ -12,5 +12,35 @@ interface device_if (input logic PCLK, PRESETn);
     logic                    PWRITE;
     logic                    PREADY;
     logic                    PSLVERR;
+        
+    clocking driver_cb @(posedge PCLK);
+        default input #1 output #1;
+        output  PSEL;
+        output  PENABLE;
+        output  PADDR;
+        output  PSTRB;
+        output  PWDATA;
+        input   PRDATA;
+        output  PWRITE;
+        input   PREADY;
+        input   PSLVERR;  
+    endclocking
     
+    clocking monitor_cb @(posedge PCLK);
+        default input #1 output #1;
+        input   PSEL;
+        input   PENABLE;
+        input   PADDR;
+        input   PSTRB;
+        input   PWDATA;
+        input   PRDATA;
+        input   PWRITE;
+        input   PREADY;
+        input   PSLVERR;  
+    endclocking
+
+    modport DRIVER  (clocking driver_cb, input PCLK, PRESETn);
+  
+    modport MONITOR (clocking monitor_cb, input PCLK, PRESETn);
+
 endinterface
--- bench/UVM/testbench.sv
+++ bench/UVM/testbench.sv
@@ -3,47 +3,14 @@ import uvm_pkg::*;
 module testbench;
     parameter PDATA_SIZE        = 8;
 
-    /////////////////////////////////////////////////////////
-    //
-    // Variables
-    //
-    //APB signals
-    logic                    PSEL;
-    logic                    PENABLE;
-    logic [             3:0] PADDR;
-    logic [PDATA_SIZE/8-1:0] PSTRB;
-    logic [PDATA_SIZE  -1:0] PWDATA;
-    logic [PDATA_SIZE  -1:0] PRDATA;
-    logic                    PWRITE;
-    logic                    PREADY;
-    logic                    PSLVERR;
-  
+    bit PCLK, PRESETn;
+    
     //GPIOs
     logic [PDATA_SIZE -1:0] gpio_o, gpio_i, gpio_oe;
   
     //IRQ
     logic irq_o;
-  
-  
-    /////////////////////////////////////////////////////////
-    //
-    // Clock & Reset
-    //
-    bit PCLK, PRESETn;
-    initial begin : gen_PCLK
-        PCLK <= 1'b0;
-        forever #10 PCLK = ~PCLK;
-    end : gen_PCLK
-  
-    initial begin : gen_PRESETn;
-      PRESETn = 1'b1;
-      //ensure falling edge of PRESETn
-      #10;
-      PRESETn = 1'b0;
-      #32;
-      PRESETn = 1'b1;
-    end : gen_PRESETn;
-
+    
     device_if device_interface(PCLK, PRESETn);
 
     apb_gpio DUT (
@@ -60,17 +27,28 @@ module testbench;
       .PSLVERR(device_interface.PSLVERR),
       .*
     );
+    
+      /////////////////////////////////////////////////////////
+      //
+      // Clock & Reset
+      //
+      initial begin : gen_PCLK
+          PCLK <= 1'b0;
+          forever #10 PCLK = ~PCLK;
+      end : gen_PCLK
+    
+      initial begin : gen_PRESETn;
+        PRESETn = 1'b1;
+        //ensure falling edge of PRESETn
+        #10;
+        PRESETn = 1'b0;
+        #32;
+        PRESETn = 1'b1;
   
+        uvm_config_db#(virtual device_if)::set(uvm_root::get(),"*","vif",device_interface);
+        //run_test("mytest");
   
-    /////////////////////////////////////////////////////////
-    //
-    // Instantiate the TB and DUT
-    //
-    // test #( .PDATA_SIZE ( PDATA_SIZE ))
-    // tb ( .* );
-  
-  
-    // apb_gpio #( .PDATA_SIZE ( PDATA_SIZE ))
-    // dut ( .* );
-   
+      end : gen_PRESETn;
+      //////////////////////////////////////////////////////////
+      
   endmodule : testbench
