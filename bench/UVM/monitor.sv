ed65f8c9346dd16bff187cbafdff7c9fdbfbd8ad
AleksaHeler <aleksaheler@gmail.com>
2022-04-12 15:43:31 +0200

    Update monitor code

--- bench/UVM/monitor.sv
+++ bench/UVM/monitor.sv
@@ -20,30 +20,44 @@ class apb_monitor extends uvm_monitor;
     // Virtual interface to DUT
     virtual device_if vif;
 
-    // // In build phase: get interface, create monitor analysis port
-    // virtual function void build_phase(uvm_phase phase);
-    //     super.build_phase(phase);
-    //     if(!uvm_config_db#(virtual device_if)::get(this, "", "apb_vif", vif))
-    //         `uvm_fatal("MON", "Could not get vif")
-    //     mon_analysis_port = new ("mon_analysis_port", this);
-    // endfunction
+    // In build phase: get interface, create monitor analysis port
+    virtual function void build_phase(uvm_phase phase);
+        super.build_phase(phase);
+        if(!uvm_config_db#(virtual device_if)::get(this, "", "apb_vif", vif))
+            `uvm_fatal("MON", "Could not get vif")
+        mon_analysis_port = new ("mon_analysis_port", this);
+    endfunction
 
-    // // This task monitors the interface for a complete
-    // // transaction and writes into analysis port when complete
-    // virtual task run_phase(uvm_phase phase);
-    //     super.run_phase(phase);
-    //     forever begin
-    //         @(MON_IF);
-    //             `uvm_info("DRV", $sformatf("Inside run_phase function"), UVM_HIGH)
-    //             // TODO: read right data
-    //             //if(vif.rstn) begin
-    //                 //Item item = Item::type_id::create("item");
-    //                 //item.in = vif.in;
-    //                 //item.out = vif.monitor_cb.out;
-    //                 //mon_analysis_port.write(item);
-    //                 //`uvm_info("MON", $sformatf("Saw items %s", item.convert2str()), UVM_HIGH)
-    //             //end
-    //     end
-    // endtask
+    // This task monitors the interface for a complete
+    // transaction and writes into analysis port when complete
+    virtual task run_phase(uvm_phase phase);
+        super.run_phase(phase);
+        forever begin
+            // Wait until PSEL is 1
+            while(!MON_IF.PSEL) @(MON_IF);
+            
+            // Create empty sequence item
+            Item item = Item::type_id::create("item");
+
+            // Populate sequence item with APB bus data
+            item.WRITE = MON_IF.PWRITE;
+            item.ADDR = MON_IF.PADDR;
+            item.STRB = MON_IF.PSTRB;
+
+            // Wait until peripheral is ready
+            while(!DRIV_IF.PREADY) @(DRIV_IF);
+            
+            // Populate sequence item with GPIO data
+            item.DATA = MON_IF.PWDATA;
+            item.gpio_o = MON_IF.gpio_o;
+            item.gpio_i = MON_IF.gpio_i;
+            item.gpio_oe = MON_IF.gpio_oe;
+
+            // wait until psel is 0
+            while(!DRIV_IF.PSEL) @(DRIV_IF);
+
+            // TODO: add print here for debugging
+        end
+    endtask
     
 endclass: apb_monitor
